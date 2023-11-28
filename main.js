87e3be375782b9934ed0b6805502f517598b2469
davitpra <davitpra@Davit>
2023-11-28 14:14:49 -0500

    final lab 6 frontend developer

---
+++ main.js
@@ -0,0 +1,14 @@
+//--------TYPING EFFECT IN THE ABOUT US SECTION---------//
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('A safe community')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
